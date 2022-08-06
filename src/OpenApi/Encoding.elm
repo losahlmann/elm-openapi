ab7242d19b083bd5d4592c5151e92c9fdb79dbb4
losahlmann <github@sahlmann.email>
2022-08-06 22:25:08 +0200

    Encoding Object.

---
+++ src/OpenApi/Encoding.elm
@@ -0,0 +1,29 @@
+module OpenApi.Encoding exposing (Encoding, jsonDecoder)
+
+import Dict exposing (Dict)
+import Json.Decode as Json
+import Json.Decode.Pipeline as JsonField
+import OpenApi.Header as Header exposing (Header)
+
+
+type alias Encoding =
+    { contentType : Maybe String
+    , headers : Dict String Header
+    , style : Maybe String
+    , explode : Bool
+    , allowReserved : Bool
+    }
+
+
+jsonDecoder : Json.Decoder Encoding
+jsonDecoder =
+    Json.succeed Encoding
+        |> JsonField.optional "contentType" (Json.nullable Json.string) Nothing
+        |> JsonField.required "headers" (Json.dict Header.jsonDecoder)
+        |> JsonField.optional "style" (Json.nullable Json.string) Nothing
+        |> JsonField.optional "explode" Json.bool True
+        |> JsonField.optional "allowReserved" Json.bool False
+
+
+
+-- TODO: tests (in spec YAML only)
